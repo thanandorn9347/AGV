9b504e1943608fae7dddc8dd02c2b4ddcbce3ffd
big9347 <thanandorn.51.6@gmail.com>
2023-02-12 02:18:02 +0700

    ros2_control_slam_sim

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,8 +2,8 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+from launch.actions import RegisterEventHandler,TimerAction
+from launch.event_handlers import OnProcessStart
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -26,13 +26,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    gazebo_params_path = os.path.join(
-        get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path}.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -40,24 +40,29 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'AGV'],
                         output='screen')
-
-    # diff_drive_spawner = Node(
-    #                     package="controller_manager",
-    #                     executable="spawner.py",
-    #                     arguments=["diff_cont"],
-    #                     )
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    # event_handler=OnProcessExit(
-    #     target_action=spawn_entity,
-    #     on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    # joint_broad_spawner = Node(
-    #                     package="controller_manager",
-    #                     executable="spawner.py",
-    #                     arguments=["joint_broad"],
-    #             )       
-
+    delayed_spawn_entity = TimerAction(period=3.0, actions=[spawn_entity])
+    diff_drive_spawner = Node(
+                        package="controller_manager",
+                        executable="spawner.py",
+                        arguments=["diff_cont"],
+                        )
+    delayed_diff_drive_spawner = RegisterEventHandler(
+    event_handler=OnProcessStart(
+        target_action=spawn_entity,
+        on_start=[diff_drive_spawner],
+        )
+    )
+    joint_broad_spawner = Node(
+                        package="controller_manager",
+                        executable="spawner.py",
+                        arguments=["joint_broad"],
+                )       
+    delayed_joint_broad_spawner = RegisterEventHandler(
+    event_handler=OnProcessStart(
+        target_action=spawn_entity,
+        on_start=[joint_broad_spawner],
+        )
+    )
    
 
 
@@ -66,8 +71,8 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         gazebo,
-        spawn_entity,
-        # delayed_diff_drive_spawner,
-        # joint_broad_spawner,
+        delayed_spawn_entity,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
         
     ])
