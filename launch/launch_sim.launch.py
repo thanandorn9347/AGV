9695495d7ec7d4620bae7f7545ef08ade1a0265f
big9347 <thanandorn.51.6@gmail.com>
2023-02-11 16:45:02 +0700

    ros2_control_gazebo

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,7 +2,8 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -25,11 +26,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    gazebo_params_path = os.path.join(
+        get_package_share_directory(package_name),'config','gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--verbose'}.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -38,6 +41,22 @@ def generate_launch_description():
                                    '-entity', 'AGV'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+                        package="controller_manager",
+                        executable="spawner.py",
+                        arguments=["diff_cont"],
+                        )
+    delayed_diff_drive_spawner = RegisterEventHandler(
+    event_handler=OnProcessExit(
+        target_action=spawn_entity,
+        on_exit=[diff_drive_spawner],
+        )
+    )
+    joint_broad_spawner = Node(
+                        package="controller_manager",
+                        executable="spawner.py",
+                        arguments=["joint_broad"],
+                )       
 
    
 
@@ -48,5 +67,7 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        delayed_diff_drive_spawner,
+        joint_broad_spawner,
         
     ])
